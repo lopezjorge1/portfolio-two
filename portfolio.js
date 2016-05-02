7153dccd12219d873465524a8280401e45f00eaf
Jorge <lopez.jorge1@outlook.com>
2016-05-02 01:19:29 -0400

    Add redirect functions for the contact icons

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+$("#email-icon").click(function () {
+    window.location.href = "mailto:lopez.jorge1695@gmail.com";
+});
+
+$("#github-icon").click(function () {
+    window.location.href = "https://www.github.com/lopezjorge1";
+});
+
+$("#linkedin-icon").click(function () {
+    window.location.href = "https://www.linkedin.com/in/jorgelopezjr";
+});
